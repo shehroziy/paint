c93b72f85423cfcc6504306b240f5b84d6c6f109
Shehroz <raxmatshehroz.com>
2022-10-31 09:55:34 +0500

    js code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 const canvas = document.querySelector("canvas")
 
-let ctx = canvas.getContext("2d")
+let ctx = canvas.getContext("2d"),
+    isDrawing = false
 
 window.addEventListener("load", ()=>{
     canvas.width = canvas.offsetWidth
     canvas.height = canvas.offsetHeight
 })
 
+const startDraw = ()=>{
+    isDrawing = true
+}
+
 const drawing = (e)=>{
+    if(!isDrawing) return
     ctx.lineTo(e.offsetX, e.offsetY)
     ctx.stroke()
 }
 
-
+canvas.addEventListener("mousedown", startDraw)
 canvas.addEventListener("mousemove", drawing)
